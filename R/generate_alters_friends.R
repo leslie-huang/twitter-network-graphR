c588dc23a3430a0db2970717a320c362b5f40879
leslie-huang <lesliehuang@gmail.com>
2017-04-10 11:27:30 -0400

    Added new functions to scrape friends

---
+++ R/generate_alters_friends.R
@@ -0,0 +1,82 @@
+
+#' Function to get friends of friends from a previously generated ego's list of friends
+#'
+#' Get networks of each alter who is friends with the original ego
+#' @param ego_network name of the ego network df (to minimize the number of API calls, you must pass in the ego network, not simply the ego user's name)
+#' @param degree_n degree that these friends are connected to the original ego
+#' @param n_alters number of ego's alters to retrieve friends for (set to NULL to retrieve friends for all alters. Value of NULL is likely to result in extremely long run time)
+#' @param n_alters_alters max num of each alter's friends to retrieve (set to NULL to retrieve all friends for each alter. NB: Value of NULL is likely to result in extremely long run time)
+#' @export
+#' @import twitteR
+#'
+#' @examples generate_alters_friends(APSA_network)
+
+generate_alters_friends <- function(ego_network, degree_n, n_alters = NULL, n_alters_alters = NULL) {
+
+  # instantiate the dataframe
+  df <- data.frame(user = character(), friend_name = character(), degree_n = character())
+
+  # if not retrieving ALL of the ego's alters
+  if (!is.null(n_alters)) {
+    num_friends <- length(ego_network$friend_name)
+
+    # check that ego has > 0 alters
+    if (num_friends > 0) {
+
+      # retrieve the smaller of (1) num ego's alters (2) n_alters parameter
+      if (num_friends < n_alters) {
+        n_alters <- num_friends
+      }
+    }
+    # print warning and exit if the ego user has no alters
+    else {
+      print("Warning: the ego user you supplied has no friends.")
+      return(df)
+    }
+
+
+
+  }
+
+  # get character vector of n alters from the ego's df
+  alters <- unique(as.character(ego_network[1:n_alters, "friend_name"]))
+
+  # generate list of user objects for the alters
+  alters_users <- lookupUsers(alters)
+
+  # get followers of each alter in alters_users
+  for (i in 1:length(alters_users)) {
+    current_alter <- alters_users[[i]]
+
+    # get current rate limit info
+    current_calls_remaining <- as.numeric(getCurRateLimitInfo()[, 3])
+
+    # Check no more API calls are left; wait 15 min
+    if (0 %in% unique(current_calls_remaining)) {
+      print("Sleeping...")
+
+      Sys.sleep(15 * 60)
+    }
+
+    # proceed only if current user is not a private account AND user has > 0 friends
+    if (current_alter$protected == FALSE && current_alter$friendsCount > 0) {
+
+      print(paste("Getting friends of", as.character(current_alter$screenName), sep = " "))
+
+      # get the followers
+      alter_friends <- current_alter$getFriends(n = n_alters_alters)
+
+      # append the followee-follower edges to the df
+      for (i in 1:length(alter_friends)) {
+        df <- rbind(df, data.frame(user = current_alter$screenName,
+                                   friend_name = alter_friends[[i]]$screenName, degree_n = degree_n))
+
+      }
+
+    }
+
+  }
+
+  return(df)
+
+}
---
+++ R/generate_ego_friend_edgelist.R
@@ -0,0 +1,43 @@
+
+#' Function to get all friends of an ego account and return a dataframe of all directed edges between ego and friends
+#'
+#' @param ego ego username
+#' @param n number of friends to retrieve, default of NULL will retrieve all followers. If a user has fewer than n friends, all friends will be retrieved
+#' @export
+#' @import twitteR
+#'
+#' @examples generate_ego_friends_edgelist("APSAtweets")
+
+generate_ego_follower_edgelist <- function(ego_name, n = NULL) {
+
+  df <- data.frame(user = character(), friend_name = character(), degree_n = character()) # instantiate a df to populate with follower edges
+
+  ego <- getUser(ego_name)
+
+  # check the user's number of followers and terminate if the user has no followers
+  friends_count <- ego$friendsCount
+
+  if (friends_count == 0) {
+    print("Warning: This user has no friends.")
+    return(df)
+  }
+
+  # if the user has supplied n, retrieve n or the total number of followers the user has (whichever is smaller)
+  if (!is.null(n)) {
+    if (n > friends_count) {
+      n <- friends_count
+    }
+  }
+
+  friends <- ego$getFriends(n = n)
+
+  num_edges <- length(followers)
+
+  for (i in 1:num_edges) {
+    df <- rbind(df, data.frame(user = ego$screenName,
+                               friend_name = friends[[i]]$screenName, degree_n = "1"))
+  }
+
+  return(df)
+
+}
